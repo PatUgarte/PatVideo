844ed5c4c177b42268febe67f1f4fabeba87fe0c
Pat Ugarte <patriciomugarte@gmail.com>
2020-05-08 22:52:47 -0300

    Instalacion y configuracion de Redux, incorporado el Provider, creado el Store y conectado el Reducer y el State.

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,35 +1,43 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 
-import useInitialState from "../hooks/useInitialState";
-
 import "../assets/styles/App.scss";
 
-const API = "http://localhost:3000/initialState";
+const Home = ({ myList, trends, originals }) => {
 
-const Home = () => {
-
-    const [videos, categories] = useInitialState(API);
+    const lists = [myList, trends, originals];
+    const categories = ["Mi Lista", "Tendencias", "Originales de PatVideo"];
 
     return (
         <>
             <SearchBar />
-            {categories.map((category) => (
-                videos[category].length > 0 && (
-                    <Categories key={category} title={category}>
-                        <Carousel>
-                            {videos[category].map((item) => (
-                                <CarouselItem key={item.id} {...item} />
-                            ))}
-                        </Carousel>
-                    </Categories>
-                )))}
+            {
+                categories.map((category, i) => (
+                    lists[i].length > 0 && (
+                        <Categories key={category} title={category}>
+                            <Carousel>
+                                {lists[i].map((item) => (
+                                    <CarouselItem key={item.id} {...item} />
+                                ))}
+                            </Carousel>
+                        </Categories>
+                    )))
+            }
         </>
     );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        myList: state.myList,
+        trends: state.trends,
+        originals: state.originals,
+    };
+};
+
+export default connect(mapStateToProps, null)(Home);
